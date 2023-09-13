31b48b9e864bc935d5e136a233a5f5ad432398ae
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-09-13 18:07:23 +0300

    <Server> created user model

---
+++ Server/models/user.model.js
@@ -0,0 +1,61 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+	{
+		first_name: {
+			type: String,
+			trim: true,
+			required: true,
+		},
+		last_name: {
+			type: String,
+			trim: true,
+			required: true,
+		},
+		username: {
+			type: String,
+			trim: true,
+			index: true,
+			unique: true,
+			required: true,
+		},
+		email: {
+			type: String,
+			trim: true,
+			index: true,
+			unique: true,
+			sparse: true,
+		},
+		password: {
+			type: String,
+			trim: true,
+			required: true,
+		},
+		user_type: {
+			type: String,
+			trim: true,
+			required: true,
+		},
+		group_id: {
+			type: String,
+			trim: true,
+		},
+		image: {
+			type: String,
+			trim: true,
+		},
+		device_id: {
+			type: String,
+			trim: true,
+			unique: true,
+			sparse: true,
+		}
+	},
+	{
+		timestamps: true,
+	}
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
