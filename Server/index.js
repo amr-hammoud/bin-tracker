71c4a1614faa270d7bbf4ea1c6e03229430debee
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-09-14 19:59:19 +0300

    <Server> updated the server

---
+++ Server/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const mongooseConnect = require("./configs/mongoDB.connect");
+const cors = require("cors");
+require("dotenv").config();
+app.use(cors());
+app.use(express.json());
+
+const authRouter = require("./routes/auth.routes")
+app.use("/auth", authRouter)
+
+const groupRouter = require("./routes/group.routes")
+app.use("/groups", groupRouter)
+
+const userTypeRouter = require("./routes/user-type.routes")
+app.use("/user-types", userTypeRouter)
+
+
+app.listen(8000, (err) => {
+	if (err) {
+		console.error("\u001b[1;34m⚡[server]: " + `\u001b[0m${err}`);
+		return;
+	}
+	console.log("\u001b[1;34m⚡[server]: " + `\u001b[0mApp Running on http://localhost:${8000}`);
+	mongooseConnect();
+});
