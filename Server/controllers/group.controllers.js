65e5897ff614d2b57056408333d3992be179a1d2
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-09-16 17:01:03 +0300

    <Server> created all group controllers

--- Server/controllers/group.controllers.js
+++ Server/controllers/group.controllers.js
@@ -21,4 +21,15 @@ const createGroup = async (req, res) => {
 	}
 };
 
-module.exports = { createGroup };
+const getAllGroups = async (req, res) => {
+	const groups = await Group.find();
+	res.status(200).send(groups);
+}
+
+const deleteGroup = async (req, res) => {
+	const id = req.body.id;
+	await Group.deleteOne({ _id: id});
+	res.status(200).send("Group deleted successfully")
+}
+
+module.exports = { createGroup, getAllGroups, deleteGroup };
