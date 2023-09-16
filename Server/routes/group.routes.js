541b32e548196df480653e771d4b6a33010bd1fe
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-09-16 17:01:21 +0300

    <Server> added group routes

--- Server/routes/group.routes.js
+++ Server/routes/group.routes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const groupController = require("../controllers/group.controllers");
 const superadminMiddleware = require("../middlewares/superadmin.middleware");
 
+router.get("/", superadminMiddleware, groupController.getAllGroups)
 router.post("/", superadminMiddleware, groupController.createGroup)
+router.delete("/", superadminMiddleware, groupController.deleteGroup)
 
 module.exports = router;
