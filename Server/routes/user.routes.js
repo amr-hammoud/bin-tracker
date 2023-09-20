7c06cd6fefc13d43974d56f0afa8c2e1ae528fcd
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-09-20 23:44:03 +0300

    <Server> updated user routes with super admin middleware

--- Server/routes/user.routes.js
+++ Server/routes/user.routes.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/user.controllers");
-const authMiddleware = require("../middlewares/auth.middleware");
+const superAdminMiddleware = require("../middlewares/superadmin.middleware");
 
-router.get("/:id?", authMiddleware, userController.getUser)
-router.put("/:id", authMiddleware, userController.updateUser)
-router.delete("/:id", authMiddleware, userController.deleteUser)
+router.get("/:id?", superAdminMiddleware, userController.getUser)
+router.put("/:id", superAdminMiddleware, userController.updateUser)
+router.delete("/:id", superAdminMiddleware, userController.deleteUser)
 
 module.exports = router;
