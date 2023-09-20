8a82e5e6365cc1acb5f17f38803b0fc868f5d566
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-09-20 23:47:55 +0300

    <Web> implemented get users function in users page

--- Web/src/dist/pages/superadmin/users/index.js
+++ Web/src/dist/pages/superadmin/users/index.js
@@ -1,23 +1,80 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const react_1 = __importDefault(require("react"));
+const react_1 = __importStar(require("react"));
 const react_redux_1 = require("react-redux");
 const sidebar_1 = __importDefault(require("../../../components/common/sidebar"));
 const navbar_1 = __importDefault(require("../../../components/common/navbar"));
+const request_1 = require("../../../configs/request");
+const listItem_1 = __importDefault(require("../../../components/base/listItem"));
 function SuperAdminUsers() {
-    const user = (0, react_redux_1.useSelector)((state) => state.auth.user);
+    const token = (0, react_redux_1.useSelector)((state) => state.auth.token);
+    const [userList, setUserList] = (0, react_1.useState)([]);
+    const getUsers = () => __awaiter(this, void 0, void 0, function* () {
+        try {
+            const response = yield (0, request_1.sendRequest)({
+                route: "users/",
+                token,
+            });
+            if (response.status === 200) {
+                setUserList(response.data);
+            }
+        }
+        catch (err) {
+            console.error(err);
+        }
+    });
+    (0, react_1.useEffect)(() => {
+        getUsers();
+    }, []);
     return (react_1.default.createElement("div", { className: "flex" },
         react_1.default.createElement(sidebar_1.default, { items: ["Dashboard", "Users", "Groups", "Account"], selected: "Users" }),
-        react_1.default.createElement("div", { className: "flex flex-col w-full" },
+        react_1.default.createElement("div", { className: "flex flex-col w-full bg-neutral-0" },
             react_1.default.createElement(navbar_1.default, { label: "Users" }),
-            react_1.default.createElement("div", { className: "p-5" },
-                react_1.default.createElement("h2", null,
-                    "Hello ", user === null || user === void 0 ? void 0 :
-                    user.first_name,
-                    " ", user === null || user === void 0 ? void 0 :
-                    user.last_name)))));
+            react_1.default.createElement("div", { className: "p-10" },
+                react_1.default.createElement("h2", null, userList.map((user, key) => {
+                    let user_type = "";
+                    if (user.user_type === "1") {
+                        user_type = "Super Admin";
+                    }
+                    else if (user.user_type === "2") {
+                        user_type = "Admin";
+                    }
+                    return react_1.default.createElement(listItem_1.default, { items: [user._id, `${user.first_name} ${user.last_name}`, user.username, user_type] });
+                }))))));
 }
 exports.default = SuperAdminUsers;
--- Web/src/pages/superadmin/users/index.tsx
+++ Web/src/pages/superadmin/users/index.tsx
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../store/store";
-import { User } from "../../../store/interfaces";
+import { Token, User } from "../../../store/interfaces";
 import Sidebar from "../../../components/common/sidebar";
 import Navbar from "../../../components/common/navbar";
+import { sendRequest } from "../../../configs/request";
+import ListItem from "../../../components/base/listItem";
+
 
 export default function SuperAdminUsers() {
-	const user: User | null = useSelector(
-		(state: RootState) => state.auth.user
+	const token: Token | null = useSelector(
+		(state: RootState) => state.auth.token
 	);
+
+	const [userList,setUserList] = useState([])
+
+	const getUsers = async () => {
+		try {
+			const response = await sendRequest({
+				route: "users/",
+				token,
+			});
+			if (response.status === 200) {
+				setUserList(response.data);
+				
+			}
+		} catch (err: any) {
+			console.error(err);
+		}
+	}
+
+	useEffect(() => {
+		getUsers()
+	}, [])
+	
 
 	return (
 		<div className="flex">
@@ -16,11 +41,20 @@ export default function SuperAdminUsers() {
 				items={["Dashboard", "Users", "Groups", "Account"]}
 				selected="Users"
 			/>
-			<div className="flex flex-col w-full">
+			<div className="flex flex-col w-full bg-neutral-0">
 				<Navbar label="Users" />
-				<div className="p-5">
+				<div className="p-10">
 					<h2>
-						Hello {user?.first_name} {user?.last_name}
+						{userList.map((user: User, key) => {
+							let user_type: string = "";
+							if(user.user_type === "1"){
+								user_type = "Super Admin"
+							}
+							else if(user.user_type === "2") {
+								user_type = "Admin"
+							}
+							return <ListItem items={[user._id, `${user.first_name} ${user.last_name}`,user.username,user_type]}/>
+						})}
 					</h2>
 				</div>
 			</div>
