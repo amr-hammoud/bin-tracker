9510f95cbf418386196e1d97d26d42c0d7ebdc2a
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-09-21 22:31:01 +0300

    <Web> updated input component with required prop

--- Web/src/components/base/Input/index.tsx
+++ Web/src/components/base/Input/index.tsx
@@ -7,6 +7,7 @@ interface InputProps {
 	icon?: React.ReactNode;
 	placeholder?: string;
 	error?: string;
+	required?: boolean;
 	inputHeight?: string;
 	onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
@@ -16,13 +17,14 @@ export default function Input(props: InputProps) {
 		<div className="flex flex-wrap flex-col justify-center content-center font-poppins h-fit my-1 text-gunmetal">
 			<div className=" text-sm flex content-center gap-1">
 				<div className=" flex content-center flex-wrap text-primary-500">{props.icon}</div>
-				<div className=" flex content-center flex-wrap">{props.label}</div>
+				<div className=" flex content-center flex-wrap">{props.label}{props.required ? <span className=" text-red-500">*</span> : ""}</div>
 			</div>
 			<input
 				type={props.type || "text"}
 				name={props.name}
 				className={`peer/${props.name} rounded w-full text-base
                         bg-neutral-50 border-neutral-700
+						placeholder:text-neutral-700 placeholder:text-base
                         focus:ring-primary-500 focus:border-primary-500
                         focus:invalid:border-red-500 focus:invalid:ring-red-500  ${props.inputHeight ? props.inputHeight : " h-11" }`}
 				placeholder={props.placeholder}
