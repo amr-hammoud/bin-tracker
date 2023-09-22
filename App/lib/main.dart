e78edbd92e240f509fcf69ef67b3610566e2dc15
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-09-22 23:47:28 +0300

    <App> updated main page

--- App/lib/main.dart
+++ App/lib/main.dart
@@ -1,7 +1,6 @@
-import 'dart:convert';
-
+import 'package:bin_tracker_flutter/auth.dart';
+import 'package:bin_tracker_flutter/home.dart';
 import 'package:flutter/material.dart';
-import 'package:dio/dio.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,128 +14,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Bin Tracker',
       theme: ThemeData(
-        colorScheme:
-            ColorScheme.fromSeed(
+        colorScheme: ColorScheme.fromSeed(
             seedColor: const Color.fromARGB(255, 70, 119, 255)),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  String _emailOrUsername = "";
-  String _email = "";
-  String _username = "";
-  String _password = "";
-
-  void _login() {
-    final form = _formKey.currentState;
-    print("Hello");
-    if (form != null && form.validate()) {
-      form.save();
-
-      if (_emailOrUsername.contains("@")) {
-        _email = _emailOrUsername;
-        _username = "";
-      } else {
-        _username = _emailOrUsername;
-        String email = "";
-      }
-
-      final dio = Dio();
-
-      final data = {
-        'email': _email,
-        'username': _username,
-        'password': _password,
-      };
-
-      dio
-          .post('http://localhost:8000/auth/login', data: data)
-          .then((response) {
-        print("Response: $response");
-        if (response.statusCode == 200) {
-          final responseData = jsonDecode(response.data);
-          String token = responseData['token'];
-          print("Token: $token");
-        } else {
-          print("Login Failed");
-        }
-      }).catchError((err) {
-        print("Error: $err");
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-          child: Form(
-        key: _formKey,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            // Padding(
-            //     padding: EdgeInsets.all(20.0),
-            //     child: Image(image: AssetImage('./assets/logo.png'))),
-            const Text(
-              "Login",
-              style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: TextFormField(
-                keyboardType: TextInputType.emailAddress,
-                decoration: const InputDecoration(
-                  labelText: "Email/Username",
-                  border: OutlineInputBorder(),
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your email/username';
-                  }
-                  return null;
-                },
-                onSaved: (newValue) {
-                  _emailOrUsername = newValue ?? "";
-                },
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: TextFormField(
-                obscureText: true,
-                decoration: const InputDecoration(
-                  labelText: "Password",
-                  border: OutlineInputBorder(),
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your password';
-                  }
-                  return null;
-                },
-                onSaved: (newValue) {
-                  _password = newValue ?? "";
-                },
-              ),
-            ),
-            ElevatedButton(onPressed: _login, child: const Text("Login"))
-          ],
-        ),
-      )),
+      home: const AuthPage(),
+      routes: {
+        '/auth': (context) => const AuthPage(),
+        '/home': (context) => const HomePage(),
+      },
     );
   }
 }
