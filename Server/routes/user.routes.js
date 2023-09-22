4db04221ca406e1706427f50cec13db530603e95
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-09-22 08:40:31 +0300

    <Server> updated user routes

--- Server/routes/user.routes.js
+++ Server/routes/user.routes.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/user.controllers");
 const superAdminMiddleware = require("../middlewares/superadmin.middleware");
+const authMiddleware = require("../middlewares/auth.middleware");
 
+router.get("/group/:id?", authMiddleware, userController.getGroupUser)
 router.get("/:id?", superAdminMiddleware, userController.getUser)
 router.put("/:id", superAdminMiddleware, userController.updateUser)
 router.delete("/:id", superAdminMiddleware, userController.deleteUser)
