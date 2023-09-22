dd5c2e41d789384cfc59d869d92d98200c19cdab
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-09-22 08:58:05 +0300

    <Web> added fetch trucks function to trucks page

--- Web/src/dist/pages/admin/trucks/index.js
+++ Web/src/dist/pages/admin/trucks/index.js
@@ -1,14 +1,67 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const react_1 = __importDefault(require("react"));
+const react_1 = __importStar(require("react"));
 const react_redux_1 = require("react-redux");
 const sidebar_1 = __importDefault(require("../../../components/common/sidebar"));
 const navbar_1 = __importDefault(require("../../../components/common/navbar"));
+const listheader_1 = __importDefault(require("../../../components/base/listheader"));
+const request_1 = require("../../../configs/request");
+const listItem_1 = __importDefault(require("../../../components/base/listItem"));
 function AdminTrucks() {
-    const user = (0, react_redux_1.useSelector)((state) => state.auth.user);
+    const token = (0, react_redux_1.useSelector)((state) => state.auth.token);
+    const [trucksList, setTrucksList] = (0, react_1.useState)([]);
+    const getTrucks = () => __awaiter(this, void 0, void 0, function* () {
+        try {
+            const response = yield (0, request_1.sendRequest)({
+                route: "trucks/",
+                token,
+            });
+            if (response.status === 200) {
+                setTrucksList(response.data);
+            }
+        }
+        catch (err) {
+            console.error(err);
+        }
+    });
+    (0, react_1.useEffect)(() => {
+        getTrucks();
+    }, []);
     return (react_1.default.createElement("div", { className: "flex" },
         react_1.default.createElement(sidebar_1.default, { items: [
                 "Dashboard",
@@ -22,11 +75,15 @@ function AdminTrucks() {
             ], selected: "Trucks" }),
         react_1.default.createElement("div", { className: "flex flex-col w-full" },
             react_1.default.createElement(navbar_1.default, { label: "Trucks" }),
-            react_1.default.createElement("div", { className: "p-5" },
-                react_1.default.createElement("h2", null,
-                    "Hello ", user === null || user === void 0 ? void 0 :
-                    user.first_name,
-                    " ", user === null || user === void 0 ? void 0 :
-                    user.last_name)))));
+            react_1.default.createElement("div", { className: "p-10" },
+                react_1.default.createElement(listheader_1.default, { items: ["Plate Number", "Driver", "Last Oil Change", "Last Wash", "Actions"] }),
+                trucksList.map((truck, index) => {
+                    return (react_1.default.createElement(listItem_1.default, { key: index, items: [
+                            truck.plate_number,
+                            truck.driver_id.first_name + " " + truck.driver_id.last_name,
+                            truck.last_oil_change,
+                            truck.last_wash,
+                        ] }));
+                })))));
 }
 exports.default = AdminTrucks;
--- Web/src/pages/admin/trucks/index.tsx
+++ Web/src/pages/admin/trucks/index.tsx
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../store/store";
-import { User } from "../../../store/interfaces";
+import { Token, Truck } from "../../../store/interfaces";
 import Sidebar from "../../../components/common/sidebar";
 import Navbar from "../../../components/common/navbar";
+import ListHeader from "../../../components/base/listheader";
+import { sendRequest } from "../../../configs/request";
+import ListItem from "../../../components/base/listItem";
 
 export default function AdminTrucks() {
-	const user: User | null = useSelector(
-		(state: RootState) => state.auth.user
+	const token: Token | null = useSelector(
+		(state: RootState) => state.auth.token
 	);
+
+	const [trucksList, setTrucksList] = useState<Truck[]>([]);
+
+	const getTrucks = async () => {
+		try {
+			const response = await sendRequest({
+				route: "trucks/",
+				token,
+			});
+			if (response.status === 200) {
+				setTrucksList(response.data);
+			}
+		} catch (err: any) {
+			console.error(err);
+		}
+	}
+
+	useEffect(() => {
+		getTrucks();
+	}, []);
 
 	return (
 		<div className="flex">
@@ -27,10 +50,23 @@ export default function AdminTrucks() {
 			/>
 			<div className="flex flex-col w-full">
 				<Navbar label="Trucks" />
-				<div className="p-5">
-					<h2>
-						Hello {user?.first_name} {user?.last_name}
-					</h2>
+				<div className="p-10">
+					<ListHeader
+						items={["Plate Number", "Driver", "Last Oil Change", "Last Wash", "Actions"]}
+					/>
+					{trucksList.map((truck: Truck, index) => {
+						return (
+							<ListItem
+								key={index}
+								items={[
+									truck.plate_number,
+									truck.driver_id.first_name + " " + truck.driver_id.last_name,
+									truck.last_oil_change,
+									truck.last_wash,
+								]}
+							/>
+						);
+					})}
 				</div>
 			</div>
 		</div>
