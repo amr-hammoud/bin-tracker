c4513dd66a631db06a3f425b6dfc0baee5a21569
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-09-22 09:06:48 +0300

    <Web> added fetch users function to users page

--- Web/src/pages/admin/users/index.tsx
+++ Web/src/pages/admin/users/index.tsx
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../store/store";
-import { User } from "../../../store/interfaces";
+import { Token, User } from "../../../store/interfaces";
 import Sidebar from "../../../components/common/sidebar";
 import Navbar from "../../../components/common/navbar";
+import { sendRequest } from "../../../configs/request";
+import ListHeader from "../../../components/base/listheader";
+import ListItem from "../../../components/base/listItem";
 
 export default function AdminUsers() {
-	const user: User | null = useSelector(
-		(state: RootState) => state.auth.user
+	const token: Token | null = useSelector(
+		(state: RootState) => state.auth.token
 	);
+
+	const [userList, setUserList] = useState<User[]>([]);
+
+	const getTrucks = async () => {
+		try {
+			const response = await sendRequest({
+				route: "users/group",
+				token,
+			});
+			if (response.status === 200) {
+				setUserList(response.data);
+			}
+		} catch (err: any) {
+			console.error(err);
+		}
+	}
+
+	useEffect(() => {
+		getTrucks();
+	}, []);
 
 	return (
 		<div className="flex">
@@ -27,10 +50,30 @@ export default function AdminUsers() {
 			/>
 			<div className="flex flex-col w-full">
 				<Navbar label="Users" />
-				<div className="p-5">
-					<h2>
-						Hello {user?.first_name} {user?.last_name}
-					</h2>
+				<div className="p-10">
+					<ListHeader
+						items={["ID", "Name", "Username", "Role", "Actions"]}
+					/>
+					{userList.map((user: User, key) => {
+						let user_type: string = "";
+						if (user.user_type === "1") {
+							user_type = "Super Admin";
+						} else if (user.user_type === "2") {
+							user_type = "Admin";
+						} else if (user.user_type === "3") {
+							user_type = "Driver";
+						}
+						return (
+							<ListItem
+								items={[
+									user._id,
+									`${user.first_name} ${user.last_name}`,
+									user.username,
+									user_type,
+								]}
+							/>
+						);
+					})}
 				</div>
 			</div>
 		</div>
