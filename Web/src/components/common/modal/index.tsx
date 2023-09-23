bcdfe6053adb3892f3cab74d3ec800fc27face9b
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-09-23 17:51:14 +0300

    <Web> created the modal component

---
+++ Web/src/components/common/modal/index.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import Modal from "react-modal";
+
+interface ModalProps {
+	showModal: boolean;
+	onRequestClose: () => void;
+	children: React.ReactNode;
+}
+
+const customStyles: Modal.Styles = {
+	content: {
+		top: "50%",
+		left: "50%",
+		height: "fit-content",
+		width: "fit-content",
+		transform: "translate(-50%, -50%)",
+		backgroundColor: "#fff",
+		borderRadius: "10px",
+		boxShadow: "0 0 10px rgba(0,0,0,0.5)",
+		padding: "20px",
+		display: "flex",
+		flexDirection: "column",
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	overlay: {
+		backgroundColor: "rgba(0,0,0,0.5)",
+		top: 0,
+		left: 0,
+		right: 0,
+		bottom: 0,
+	},
+};
+
+const ModalComponent: React.FC<ModalProps> = ({
+	showModal,
+	onRequestClose,
+	children,
+}) => {
+	return (
+		<Modal
+			isOpen={showModal}
+			shouldCloseOnOverlayClick={true}
+			onRequestClose={onRequestClose}
+			style={customStyles}
+			contentLabel="Modal"
+			appElement={document.getElementById("root") || undefined}
+		>
+			{children}
+		</Modal>
+	);
+};
+
+export default ModalComponent;
