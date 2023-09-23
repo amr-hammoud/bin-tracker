7ad35caa774c1f337cca1c1e621976b6b707b625
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-09-23 18:02:55 +0300

    <Web> added action button to navbar

--- Web/src/components/common/navbar/index.tsx
+++ Web/src/components/common/navbar/index.tsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React from "react";
 
 interface NavbarProps {
-    label?: string
+	label?: string;
+    buttonLabel?: string;
+    buttonAction?: () => void;
 }
 
-export default function Navbar (props: NavbarProps) {
-    return (
-        <div className='px-5 py-3 font-semibold text-2xl text-neutral-0 bg-primary-500 w-full select-none'>
-            {props.label}
-        </div>
-    );
+export default function Navbar(props: NavbarProps) {
+	return (
+		<div className="flex justify-between px-5 py-3 font-semibold text-2xl text-neutral-0 bg-primary-500 w-full select-none">
+			{props.label}
+            {props.buttonAction ? 
+			<div className="flex flex-wrap justify-center content-center py-1 px-2
+                            rounded text-base shadow-md bg-neutral-0 text-primary-500"
+                    onClick={() => props.buttonAction ? props.buttonAction() : ""}>
+				{props.buttonLabel}
+			</div> : ""}
+		</div>
+	);
 }
--- Web/src/dist/components/common/navbar/index.js
+++ Web/src/dist/components/common/navbar/index.js
@@ -5,6 +5,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const react_1 = __importDefault(require("react"));
 function Navbar(props) {
-    return (react_1.default.createElement("div", { className: 'px-5 py-3 font-semibold text-2xl text-neutral-0 bg-primary-500 w-full select-none' }, props.label));
+    return (react_1.default.createElement("div", { className: "flex justify-between px-5 py-3 font-semibold text-2xl text-neutral-0 bg-primary-500 w-full select-none" },
+        props.label,
+        props.buttonAction ?
+            react_1.default.createElement("div", { className: "flex flex-wrap justify-center content-center py-1 px-2\r\n                            rounded text-base shadow-md bg-neutral-0 text-primary-500", onClick: () => props.buttonAction ? props.buttonAction() : "" }, props.buttonLabel) : ""));
 }
 exports.default = Navbar;
