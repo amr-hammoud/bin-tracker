fedc798677e1b8827ee09e195b82c3ceeff2d020
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-09-23 15:14:39 +0300

    <App> updated user provider

--- App/lib/providers/UserProvider.dart
+++ App/lib/providers/UserProvider.dart
@@ -1,7 +1,6 @@
 import 'package:bin_tracker_flutter/classes/User.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class UserProvider with ChangeNotifier {
   User? _user;
@@ -13,15 +12,4 @@ class UserProvider with ChangeNotifier {
     notifyListeners();
   }
 
-
-  Future<bool> checkLoggedIn(BuildContext context) async {
-    final userProvider = Provider.of<UserProvider>(context, listen: false);
-
-    await Future.delayed(Duration(seconds: 2));
-
-    if (userProvider._user?.token != null) {
-      return true;
-    }
-    return false;
-  }
 }
