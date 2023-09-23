64379b65095d5ef37dc4d62de7bcdb7b6d972f53
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-09-23 18:58:01 +0300

    <Web> updated list item to accept an object instead of an array

--- Web/src/components/base/listItem/index.tsx
+++ Web/src/components/base/listItem/index.tsx
@@ -3,10 +3,10 @@ import { MdOutlineEdit } from "react-icons/md";
 import { RiDeleteBin6Line } from "react-icons/ri";
 
 interface ListItemProps {
-	items: Array<string | undefined>;
+	data: Record<string, string | undefined>;
 	customIcon?: React.ReactNode;
 	customIconAction?: () => void;
-	onEdit?: (id: string) => void;
+	onEdit?: (data: any) => void;
 	onDelete?: (id: string) => void;
 }
 
@@ -19,13 +19,12 @@ export default function ListItem(props: ListItemProps) {
                         hover:bg-primary-100"
 		>
 			<div className="flex flex-grow justify-between gap-3">
-				{props.items.map((item, index) => {
+				{Object.entries(props.data).map(([key, value], index) => {
 					return (
-						<div
-							className="w-full px-3 truncate"
-							key={index}
-						>
-							<div>{item ? item : "-"}</div>
+						<div className="w-full px-3 truncate" key={index}>
+							<div>
+								{value ? value : "-"}
+							</div>
 						</div>
 					);
 				})}
@@ -44,6 +43,10 @@ export default function ListItem(props: ListItemProps) {
 					<div
 						className="flex flex-wrap justify-between content-center opacity-70
                                 hover:cursor-pointer hover:opacity-100"
+						data-custom={JSON.stringify(props.data)}
+						onClick={(e) =>
+							props.onEdit ? props.onEdit(e.currentTarget.getAttribute("data-custom")) : ""
+						}
 					>
 						<MdOutlineEdit />
 					</div>
@@ -54,7 +57,7 @@ export default function ListItem(props: ListItemProps) {
 					<div
 						className="flex flex-wrap justify-between content-center opacity-70
                                 hover:cursor-pointer hover:opacity-100"
-						id={props.items ? props.items[0] : ""}
+						id={props.data ? props.data._id : ""}
 						onClick={(e) =>
 							props.onDelete
 								? props.onDelete(e.currentTarget.id)
