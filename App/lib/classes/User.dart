77bc5c0c29e1c0c7a097c2ca01ac904860b6fb20
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-09-23 15:15:34 +0300

    <App> added getter/setter to user model

--- App/lib/classes/User.dart
+++ App/lib/classes/User.dart
@@ -1,8 +1,20 @@
+import 'dart:convert';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+final storage = FlutterSecureStorage();
+
 class User {
   String token;
   UserData user;
 
   User({required this.token, required this.user});
+
+  String toJson() {
+    return jsonEncode({
+      'token': token,
+      'user': user.toJson(),
+    });
+  }
 
   factory User.fromJson(Map<String, dynamic> json) {
     return User(
@@ -45,4 +57,47 @@ class UserData {
       email: json['email'],
     );
   }
+
+  Map<String, dynamic> toJson() {
+    return {
+      '_id': id,
+      'first_name': firstName,
+      'last_name': lastName,
+      'username': username,
+      'user_type': userType,
+      'createdAt': createdAt.toIso8601String(),
+      'updatedAt': updatedAt.toIso8601String(),
+      'email': email,
+    };
+  }
+}
+
+Future<void> saveUser(User user) async {
+  final jsonString = user.toJson();
+  await storage.write(key: 'user', value: jsonString);
+}
+
+Future<String?> getUser(String key) async {
+  final jsonString = await storage.read(key: 'user');
+  if (jsonString != null) {
+    final Map<String, dynamic> jsonMap = jsonDecode(jsonString);
+    User user = User.fromJson(jsonMap);
+    switch (key) {
+      case "name":
+        return "${user.user.firstName} ${user.user.lastName}"
+            as Future<String?>;
+      case "firstName":
+        return user.user.firstName as Future<String?>;
+      case "lastName":
+        return user.user.lastName as Future<String?>;
+      case "username":
+        return user.user.username as Future<String?>;
+      case "userType":
+        return user.user.userType as Future<String?>;
+      case "email":
+        return user.user.email as Future<String?>;
+      default:
+    }
+  }
+  return null;
 }
