777d0c3f2b482666e89fc5683debce42fc9a4aa2
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-09-23 15:20:09 +0300

    <App> updated the main to use the bottom navbar with the provider

--- App/lib/main.dart
+++ App/lib/main.dart
@@ -1,31 +1,62 @@
-import 'package:bin_tracker_flutter/auth.dart';
-import 'package:bin_tracker_flutter/providers/UserProvider.dart';
+import 'package:bin_tracker_flutter/custom/BottomNavigation.dart';
+import 'package:bin_tracker_flutter/helpers/AuthHelpers.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'auth.dart';
+import 'providers/UserProvider.dart';
+
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+  final auth = AuthHelpers();  
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Bin Tracker',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color(0xff3DA35D)),
-        fontFamily: 'Poppins',
-        useMaterial3: true,
-      ),
-      home: ChangeNotifierProvider(
-        create: (_) => UserProvider(),
-        child: const AuthPage(),
-      ),
-      routes: {
-        '/auth': (context) => const AuthPage(),
+    return ChangeNotifierProvider(
+      create: (_) => UserProvider(),
+      builder: (context, child) {
+        return MaterialApp(
+          title: 'Bin Tracker',
+          theme: ThemeData(
+            colorScheme:
+                ColorScheme.fromSeed(seedColor: const Color(0xff3DA35D)),
+            fontFamily: 'Poppins',
+            useMaterial3: true,
+          ),
+          home: FutureBuilder<bool>(
+            future: auth.checkLoggedIn(context),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Container(
+                  width: 50,
+                  height: 50,
+                  child: const CircularProgressIndicator(
+                    valueColor:
+                        AlwaysStoppedAnimation<Color>(Color(0xff3DA35D)),
+                    backgroundColor: Colors.grey,
+                  ),
+                );
+              } else if (snapshot.hasError) {
+                return Scaffold(
+                  body: Center(
+                    child: Text('Error: ${snapshot.error.toString()}'),
+                  ),
+                );
+              } else if (snapshot.data == true) {
+                return BottomNavigation();
+              } else {
+                return AuthPage();
+              }
+            },
+          ),
+          routes: {
+            '/auth': (context) => const AuthPage(),
+          },
+        );
       },
     );
   }
