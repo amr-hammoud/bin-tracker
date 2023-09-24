446bee0fb62429850e024ef3e81f4ee7c295c49a
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-09-24 22:45:19 +0300

    <Web> added create bin feature

--- Web/src/dist/pages/admin/bins/index.js
+++ Web/src/dist/pages/admin/bins/index.js
@@ -31,6 +31,17 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
+                t[p[i]] = s[p[i]];
+        }
+    return t;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -43,8 +54,14 @@ const request_1 = require("../../../configs/request");
 const listItem_1 = __importDefault(require("../../../components/base/listItem"));
 const md_1 = require("react-icons/md");
 const listheader_1 = __importDefault(require("../../../components/base/listheader"));
+const react_hot_toast_1 = require("react-hot-toast");
+const button_1 = __importDefault(require("../../../components/base/button"));
+const select_1 = __importDefault(require("../../../components/base/select"));
+const input_1 = __importDefault(require("../../../components/base/input"));
+const modal_1 = __importDefault(require("../../../components/base/modal"));
 function AdminBins() {
     const token = (0, react_redux_1.useSelector)((state) => state.auth.token);
+    const user = (0, react_redux_1.useSelector)((state) => state.auth.user);
     const [binsList, setBinList] = (0, react_1.useState)([]);
     const getBins = () => __awaiter(this, void 0, void 0, function* () {
         try {
@@ -63,6 +80,52 @@ function AdminBins() {
     (0, react_1.useEffect)(() => {
         getBins();
     }, []);
+    const [createModalState, setCreateModalState] = (0, react_1.useState)({
+        open: false,
+        type: "create",
+    });
+    const [binData, setBinData] = (0, react_1.useState)({
+        _id: "",
+        custom_id: "",
+        longitude: "",
+        latitude: "",
+        group_id: "",
+        last_pickup_time: "",
+        waste_type: "",
+        data: [],
+    });
+    const activateCreateModal = () => {
+        setBinData(Object.assign(Object.assign({}, binData), { _id: "", custom_id: "", longitude: "", latitude: "", group_id: user === null || user === void 0 ? void 0 : user.group_id, last_pickup_time: "", waste_type: "General", data: [] }));
+        setCreateModalState(Object.assign(Object.assign({}, createModalState), { open: true, type: "create" }));
+    };
+    const createBin = () => __awaiter(this, void 0, void 0, function* () {
+        const { _id } = binData, restData = __rest(binData, ["_id"]);
+        const asArray = Object.entries(restData);
+        const filtered = asArray.filter(([key, value]) => value !== null && value !== "");
+        const finalData = Object.fromEntries(filtered);
+        try {
+            const response = yield (0, request_1.sendRequest)({
+                method: "POST",
+                route: `bins`,
+                body: finalData,
+                token,
+            });
+            if (response.status === 200) {
+                setCreateModalState(Object.assign(Object.assign({}, createModalState), { open: false }));
+                getBins();
+                react_hot_toast_1.toast.success("Truck created successfully", { duration: 2500 });
+            }
+            else {
+                setCreateModalState(Object.assign(Object.assign({}, createModalState), { open: false }));
+                react_hot_toast_1.toast.error("Couldn't Create, Try Again", { duration: 4000 });
+            }
+        }
+        catch (err) {
+            console.error(err);
+            setCreateModalState(Object.assign(Object.assign({}, createModalState), { open: false }));
+            react_hot_toast_1.toast.error("Couldn't Create, Try Again", { duration: 2500 });
+        }
+    });
     const showLocation = () => {
         console.log("Location");
     };
@@ -77,11 +140,44 @@ function AdminBins() {
                 "Chats",
                 "Account",
             ], selected: "Bins" }),
+        react_1.default.createElement(modal_1.default, { showModal: createModalState.open, onRequestClose: () => setCreateModalState(Object.assign(Object.assign({}, createModalState), { open: !createModalState.open })) },
+            react_1.default.createElement("div", { className: "text-xl" }, "Create/Edit User"),
+            react_1.default.createElement("div", { className: "flex flex-col flex-wrap justify-center content-center w-96" },
+                react_1.default.createElement("div", { className: "flex gap-5" },
+                    react_1.default.createElement(input_1.default, { label: "Custom ID", placeholder: "id", value: binData.custom_id, onChange: (e) => {
+                            setBinData(Object.assign(Object.assign({}, binData), { custom_id: e.target.value }));
+                        }, required: true })),
+                react_1.default.createElement("div", { className: "flex gap-5" },
+                    react_1.default.createElement(input_1.default, { label: "Latitude", placeholder: "latitude", value: binData.latitude, onChange: (e) => {
+                            setBinData(Object.assign(Object.assign({}, binData), { latitude: e.target.value }));
+                        }, required: true }),
+                    react_1.default.createElement(input_1.default, { label: "Longitude", placeholder: "longitude", value: binData.longitude, onChange: (e) => {
+                            setBinData(Object.assign(Object.assign({}, binData), { longitude: e.target.value }));
+                        }, required: true })),
+                react_1.default.createElement(input_1.default, { label: "Last Pickup", type: "date", value: binData.last_pickup_time, onChange: (e) => {
+                        setBinData(Object.assign(Object.assign({}, binData), { last_pickup_time: e.target.value }));
+                    }, required: true }),
+                react_1.default.createElement(select_1.default, { label: "Waste Type", required: true, value: binData.waste_type, options: {
+                        "General": "General",
+                        "Recyclables": "Recyclables",
+                        "Hazardous": "Hazardous",
+                    }, onChange: (e) => setBinData(Object.assign(Object.assign({}, binData), { waste_type: e.target.value })) })),
+            react_1.default.createElement("div", { className: "flex w-full justify-center gap-10 mt-5" },
+                react_1.default.createElement(button_1.default, { label: "Cancel", color: "text-gunmetal", bgColor: "bg-neutral-100", hoverColor: "hover:bg-neutral-600", onClick: () => setCreateModalState(Object.assign(Object.assign({}, createModalState), { open: false })) }),
+                createModalState.type === "edit" ? (react_1.default.createElement(button_1.default, { label: "Update", bgColor: "bg-primary-500", hoverColor: "hover:bg-primary-700" })) : (react_1.default.createElement(button_1.default, { label: "Create", bgColor: "bg-primary-500", hoverColor: "hover:bg-primary-700", onClick: () => createBin() })))),
         react_1.default.createElement("div", { className: "flex flex-col w-full" },
-            react_1.default.createElement(navbar_1.default, { label: "Bins" }),
+            react_1.default.createElement(navbar_1.default, { label: "Bins", buttonLabel: "+ Create Bin", buttonAction: () => activateCreateModal() }),
+            react_1.default.createElement("div", null,
+                react_1.default.createElement(react_hot_toast_1.Toaster, null)),
             react_1.default.createElement("div", { className: "p-10" },
-                react_1.default.createElement(listheader_1.default, { items: ["ID", "Waste Type", "Last pickup time", "Actions"] }),
+                react_1.default.createElement(listheader_1.default, { items: [
+                        "ID",
+                        "Waste Type",
+                        "Last pickup time",
+                        "Actions",
+                    ] }),
                 binsList.map((bin, index) => {
+                    console.log(bin);
                     return (react_1.default.createElement(listItem_1.default, { key: index, items: [
                             bin.custom_id,
                             bin.waste_type,
--- Web/src/pages/admin/bins/index.tsx
+++ Web/src/pages/admin/bins/index.tsx
@@ -1,17 +1,26 @@
 import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../store/store";
-import { Bin, Token } from "../../../store/interfaces";
+import { Bin, Token, User2 } from "../../../store/interfaces";
 import Sidebar from "../../../components/common/sidebar";
 import Navbar from "../../../components/common/navbar";
 import { sendRequest } from "../../../configs/request";
 import ListItem from "../../../components/base/listItem";
 import { MdLocationPin } from "react-icons/md";
 import ListHeader from "../../../components/base/listheader";
+import { Toaster, toast } from "react-hot-toast";
+import Button from "../../../components/base/button";
+import Select from "../../../components/base/select";
+import Input from "../../../components/base/input";
+import ModalComponent from "../../../components/base/modal";
 
 export default function AdminBins() {
 	const token: Token | null = useSelector(
 		(state: RootState) => state.auth.token
+	);
+
+	const user: User2 | null = useSelector(
+		(state: RootState) => state.auth.user as User2
 	);
 
 	const [binsList, setBinList] = useState([]);
@@ -34,6 +43,86 @@ export default function AdminBins() {
 		getBins();
 	}, []);
 
+	const [createModalState, setCreateModalState] = useState({
+		open: false,
+		type: "create",
+	});
+	const [binData, setBinData] = useState<{
+		_id: string;
+		custom_id: string;
+		longitude: string | null;
+		latitude: string | null;
+		group_id: string | undefined;
+		last_pickup_time: string | undefined;
+		waste_type: string | undefined;
+		data: Array<{
+			timestamp: string;
+			record: string;
+		}>;
+	}>({
+		_id: "",
+		custom_id: "",
+		longitude: "",
+		latitude: "",
+		group_id: "",
+		last_pickup_time: "",
+		waste_type: "",
+		data: [],
+	});
+
+	const activateCreateModal = () => {
+		setBinData({
+			...binData,
+			_id: "",
+			custom_id: "",
+			longitude: "",
+			latitude: "",
+			group_id: user?.group_id,
+			last_pickup_time: "",
+			waste_type: "General",
+			data: [],
+		});
+		setCreateModalState({
+			...createModalState,
+			open: true,
+			type: "create",
+		});
+	};
+
+	const createBin = async () => {
+		const { _id, ...restData } = binData;
+
+		const asArray = Object.entries(restData);
+
+		const filtered = asArray.filter(
+			([key, value]) => value !== null && value !== ""
+		);
+
+		const finalData = Object.fromEntries(filtered);		
+
+		try {
+			const response = await sendRequest({
+				method: "POST",
+				route: `bins`,
+				body: finalData,
+				token,
+			});
+
+			if (response.status === 200) {
+				setCreateModalState({ ...createModalState, open: false });
+				getBins();
+				toast.success("Truck created successfully", { duration: 2500 });
+			} else {
+				setCreateModalState({ ...createModalState, open: false });
+				toast.error("Couldn't Create, Try Again", { duration: 4000 });
+			}
+		} catch (err: any) {
+			console.error(err);
+			setCreateModalState({ ...createModalState, open: false });
+			toast.error("Couldn't Create, Try Again", { duration: 2500 });
+		}
+	};
+
 	const showLocation = () => {
 		console.log("Location");
 	};
@@ -53,13 +142,138 @@ export default function AdminBins() {
 				]}
 				selected="Bins"
 			/>
+			{/* Create - Edit Modal */}
+			<ModalComponent
+				showModal={createModalState.open}
+				onRequestClose={() =>
+					setCreateModalState({
+						...createModalState,
+						open: !createModalState.open,
+					})
+				}
+			>
+				<div className="text-xl">Create/Edit User</div>
+				<div className="flex flex-col flex-wrap justify-center content-center w-96">
+					<div className="flex gap-5">
+						<Input
+							label="Custom ID"
+							placeholder="id"
+							value={binData.custom_id}
+							onChange={(e) => {
+								setBinData({
+									...binData,
+									custom_id: e.target.value,
+								});
+							}}
+							required
+						/>
+					</div>
+					<div className="flex gap-5">
+						<Input
+							label="Latitude"
+							placeholder="latitude"
+							value={binData.latitude}
+							onChange={(e) => {
+								setBinData({
+									...binData,
+									latitude: e.target.value,
+								});
+							}}
+							required
+						/>
+						<Input
+							label="Longitude"
+							placeholder="longitude"
+							value={binData.longitude}
+							onChange={(e) => {
+								setBinData({
+									...binData,
+									longitude: e.target.value,
+								});
+							}}
+							required
+						/>
+					</div>
+					<Input
+							label="Last Pickup"
+							type="date"
+							value={binData.last_pickup_time}
+							onChange={(e) => {
+								setBinData({
+									...binData,
+									last_pickup_time: e.target.value,
+								});
+							}}
+							required
+						/>
+					<Select
+						label="Waste Type"
+						required
+						value={binData.waste_type}
+						options={{
+							"General": "General",
+							"Recyclables": "Recyclables",
+							"Hazardous": "Hazardous",
+						}}
+						onChange={(e) =>
+							setBinData({
+								...binData,
+								waste_type: e.target.value,
+							})
+						}
+					/>
+				</div>
+				<div className="flex w-full justify-center gap-10 mt-5">
+					<Button
+						label="Cancel"
+						color="text-gunmetal"
+						bgColor="bg-neutral-100"
+						hoverColor="hover:bg-neutral-600"
+						onClick={() =>
+							setCreateModalState({
+								...createModalState,
+								open: false,
+							})
+						}
+					/>
+					{createModalState.type === "edit" ? (
+						<Button
+							label="Update"
+							bgColor="bg-primary-500"
+							hoverColor="hover:bg-primary-700"
+							// onClick={() => updateTruck()}
+						/>
+					) : (
+						<Button
+							label="Create"
+							bgColor="bg-primary-500"
+							hoverColor="hover:bg-primary-700"
+							onClick={() => createBin()}
+						/>
+					)}
+				</div>
+			</ModalComponent>
 			<div className="flex flex-col w-full">
-				<Navbar label="Bins" />
+				<Navbar
+					label="Bins"
+					buttonLabel="+ Create Bin"
+					buttonAction={() => activateCreateModal()}
+				/>
+				<div>
+					<Toaster />
+				</div>
 				<div className="p-10">
 					<ListHeader
-						items={["ID", "Waste Type", "Last pickup time", "Actions"]}
+						items={[
+							"ID",
+							"Waste Type",
+							"Last pickup time",
+							"Actions",
+						]}
 					/>
 					{binsList.map((bin: Bin, index) => {
+						console.log(bin);
+						
 						return (
 							<ListItem
 								key={index}
