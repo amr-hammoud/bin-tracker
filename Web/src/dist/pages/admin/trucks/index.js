74e94e92f172f506ef064dd2e4aa72529a8ffecd
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-09-24 13:01:44 +0300

    <Web> added create truck feature

--- Web/src/dist/pages/admin/trucks/index.js
+++ Web/src/dist/pages/admin/trucks/index.js
@@ -31,6 +31,17 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
+                t[p[i]] = s[p[i]];
+        }
+    return t;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -45,16 +56,19 @@ const listItem_1 = __importDefault(require("../../../components/base/listItem"))
 const modal_1 = __importDefault(require("../../../components/base/modal"));
 const button_1 = __importDefault(require("../../../components/base/button"));
 const react_hot_toast_1 = require("react-hot-toast");
+const input_1 = __importDefault(require("../../../components/base/input"));
+const select_1 = __importDefault(require("../../../components/base/select"));
 function AdminTrucks() {
     const token = (0, react_redux_1.useSelector)((state) => state.auth.token);
+    const user = (0, react_redux_1.useSelector)((state) => state.auth.user);
     const [truckList, setTruckList] = (0, react_1.useState)([]);
+    const [driverList, setDriverList] = (0, react_1.useState)([]);
     const getTrucks = () => __awaiter(this, void 0, void 0, function* () {
         try {
             const response = yield (0, request_1.sendRequest)({
                 route: "trucks/",
                 token,
             });
-            console.log(response);
             if (response.status === 200) {
                 setTruckList(response.data);
             }
@@ -63,8 +77,27 @@ function AdminTrucks() {
             console.error(err);
         }
     });
+    const getDrivers = () => __awaiter(this, void 0, void 0, function* () {
+        try {
+            const response = yield (0, request_1.sendRequest)({
+                route: `users/drivers`,
+                token,
+            });
+            if (response.status === 200) {
+                setDriverList(response.data);
+            }
+            else {
+                react_hot_toast_1.toast.error("Error Getting Drivers", { duration: 4000 });
+            }
+        }
+        catch (err) {
+            console.error(err);
+            react_hot_toast_1.toast.error("Error Getting Drivers", { duration: 2500 });
+        }
+    });
     (0, react_1.useEffect)(() => {
         getTrucks();
+        getDrivers();
     }, []);
     const [deleteModalState, setDeleteModalState] = (0, react_1.useState)({
         open: false,
@@ -95,6 +128,55 @@ function AdminTrucks() {
             react_hot_toast_1.toast.error("Couldn't Delete Truck", { duration: 4000 });
         }
     });
+    const [createModalState, setCreateModalState] = (0, react_1.useState)({
+        open: false,
+        type: "create",
+    });
+    const [truckData, setTruckData] = (0, react_1.useState)({
+        _id: "",
+        custom_id: "",
+        plate_number: "",
+        group_id: "",
+        driver_id: "",
+        last_oil_change: "",
+        last_wash: "",
+    });
+    const activateCreateModal = () => {
+        setTruckData(Object.assign(Object.assign({}, truckData), { _id: "", custom_id: "", plate_number: "", group_id: user === null || user === void 0 ? void 0 : user.group_id, driver_id: driverList[0]._id, last_oil_change: "", last_wash: "" }));
+        setCreateModalState(Object.assign(Object.assign({}, createModalState), { open: true, type: "create" }));
+    };
+    const createTruck = () => __awaiter(this, void 0, void 0, function* () {
+        const { _id } = truckData, restData = __rest(truckData, ["_id"]);
+        const asArray = Object.entries(restData);
+        const filtered = asArray.filter(([key, value]) => value !== null && value !== "");
+        const finalData = Object.fromEntries(filtered);
+        try {
+            const response = yield (0, request_1.sendRequest)({
+                method: "POST",
+                route: `trucks`,
+                body: finalData,
+                token,
+            });
+            if (response.status === 200) {
+                setCreateModalState(Object.assign(Object.assign({}, createModalState), { open: false }));
+                getTrucks();
+                react_hot_toast_1.toast.success("Truck created successfully", { duration: 2500 });
+            }
+            else {
+                setCreateModalState(Object.assign(Object.assign({}, createModalState), { open: false }));
+                react_hot_toast_1.toast.error("Couldn't Create, Try Again", { duration: 4000 });
+            }
+        }
+        catch (err) {
+            console.error(err);
+            setCreateModalState(Object.assign(Object.assign({}, createModalState), { open: false }));
+            react_hot_toast_1.toast.error("Couldn't Create, Try Again", { duration: 2500 });
+        }
+    });
+    const transformedDriversList = driverList.reduce((acc, currentItem) => {
+        acc[currentItem.username] = currentItem._id;
+        return acc;
+    }, {});
     return (react_1.default.createElement("div", { className: "flex" },
         react_1.default.createElement(sidebar_1.default, { items: [
                 "Dashboard",
@@ -106,13 +188,38 @@ function AdminTrucks() {
                 "Chats",
                 "Account",
             ], selected: "Trucks" }),
+        react_1.default.createElement(modal_1.default, { showModal: createModalState.open, onRequestClose: () => setCreateModalState(Object.assign(Object.assign({}, createModalState), { open: !createModalState.open })) },
+            react_1.default.createElement("div", { className: "text-xl" }, "Create/Edit User"),
+            react_1.default.createElement("div", { className: "flex flex-col flex-wrap justify-center content-center w-96" },
+                react_1.default.createElement("div", { className: "flex gap-5" },
+                    react_1.default.createElement(input_1.default, { label: "Custom ID", placeholder: "id", value: truckData.custom_id, onChange: (e) => {
+                            setTruckData(Object.assign(Object.assign({}, truckData), { custom_id: e.target.value }));
+                            console.log(truckData);
+                        }, required: true }),
+                    react_1.default.createElement(input_1.default, { label: "Plate Number", placeholder: "plate", value: truckData.plate_number, onChange: (e) => {
+                            setTruckData(Object.assign(Object.assign({}, truckData), { plate_number: e.target.value }));
+                            console.log(truckData);
+                        }, required: true })),
+                react_1.default.createElement("div", { className: "flex gap-5" },
+                    react_1.default.createElement(input_1.default, { label: "Last Oil Change", type: "date", value: truckData.last_oil_change, onChange: (e) => {
+                            setTruckData(Object.assign(Object.assign({}, truckData), { last_oil_change: e.target.value }));
+                            console.log(truckData);
+                        }, required: true }),
+                    react_1.default.createElement(input_1.default, { label: "Last Wash", type: "date", value: truckData.last_wash, onChange: (e) => {
+                            setTruckData(Object.assign(Object.assign({}, truckData), { last_wash: e.target.value }));
+                            console.log(truckData);
+                        }, required: true })),
+                react_1.default.createElement(select_1.default, { label: "Driver", required: true, value: truckData.driver_id, options: transformedDriversList, onChange: (e) => setTruckData(Object.assign(Object.assign({}, truckData), { driver_id: e.target.value })) })),
+            react_1.default.createElement("div", { className: "flex w-full justify-center gap-10 mt-5" },
+                react_1.default.createElement(button_1.default, { label: "Cancel", color: "text-gunmetal", bgColor: "bg-neutral-100", hoverColor: "hover:bg-neutral-600", onClick: () => setCreateModalState(Object.assign(Object.assign({}, createModalState), { open: false })) }),
+                createModalState.type === "edit" ? (react_1.default.createElement(button_1.default, { label: "Update", bgColor: "bg-primary-500", hoverColor: "hover:bg-primary-700" })) : (react_1.default.createElement(button_1.default, { label: "Create", bgColor: "bg-primary-500", hoverColor: "hover:bg-primary-700", onClick: () => createTruck() })))),
         react_1.default.createElement(modal_1.default, { showModal: deleteModalState.open, onRequestClose: () => setDeleteModalState(Object.assign(Object.assign({}, deleteModalState), { open: !deleteModalState.open })) },
             react_1.default.createElement("div", { className: "text-xl" }, "Are you sure you want to delete?"),
             react_1.default.createElement("div", { className: "flex w-full justify-center gap-10 mt-5" },
                 react_1.default.createElement(button_1.default, { label: "Cancel", color: "text-gunmetal", bgColor: "bg-neutral-100", hoverColor: "hover:bg-neutral-600", onClick: () => setDeleteModalState(Object.assign(Object.assign({}, deleteModalState), { open: false })) }),
                 react_1.default.createElement(button_1.default, { label: "Delete", bgColor: "bg-red-400", hoverColor: "hover:bg-red-500", onClick: () => deleteTruck(deleteModalState.id) }))),
         react_1.default.createElement("div", { className: "flex flex-col w-full" },
-            react_1.default.createElement(navbar_1.default, { label: "Trucks" }),
+            react_1.default.createElement(navbar_1.default, { label: "Trucks", buttonLabel: "+ Create Truck", buttonAction: () => activateCreateModal() }),
             react_1.default.createElement("div", null,
                 react_1.default.createElement(react_hot_toast_1.Toaster, null)),
             react_1.default.createElement("div", { className: "p-10" },
--- Web/src/pages/admin/trucks/index.tsx
+++ Web/src/pages/admin/trucks/index.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../store/store";
-import { Token, Truck } from "../../../store/interfaces";
+import { Token, Truck, User, User2 } from "../../../store/interfaces";
 import Sidebar from "../../../components/common/sidebar";
 import Navbar from "../../../components/common/navbar";
 import ListHeader from "../../../components/base/listheader";
@@ -10,13 +10,19 @@ import ListItem from "../../../components/base/listItem";
 import ModalComponent from "../../../components/base/modal";
 import Button from "../../../components/base/button";
 import { Toaster, toast } from "react-hot-toast";
+import Input from "../../../components/base/input";
+import Select from "../../../components/base/select";
 
 export default function AdminTrucks() {
 	const token: Token | null = useSelector(
 		(state: RootState) => state.auth.token
 	);
+	const user: User2 | null = useSelector(
+		(state: RootState) => state.auth.user as User2
+	);
 
 	const [truckList, setTruckList] = useState<Truck[]>([]);
+	const [driverList, setDriverList] = useState<User[]>([]);
 
 	const getTrucks = async () => {
 		try {
@@ -24,7 +30,6 @@ export default function AdminTrucks() {
 				route: "trucks/",
 				token,
 			});
-			console.log(response);
 
 			if (response.status === 200) {
 				setTruckList(response.data);
@@ -34,8 +39,27 @@ export default function AdminTrucks() {
 		}
 	};
 
+	const getDrivers = async () => {
+		try {
+			const response = await sendRequest({
+				route: `users/drivers`,
+				token,
+			});
+
+			if (response.status === 200) {
+				setDriverList(response.data);
+			} else {
+				toast.error("Error Getting Drivers", { duration: 4000 });
+			}
+		} catch (err: any) {
+			console.error(err);
+			toast.error("Error Getting Drivers", { duration: 2500 });
+		}
+	};
+
 	useEffect(() => {
 		getTrucks();
+		getDrivers();
 	}, []);
 
 	const [deleteModalState, setDeleteModalState] = useState({
@@ -69,6 +93,89 @@ export default function AdminTrucks() {
 		}
 	};
 
+	const [createModalState, setCreateModalState] = useState({
+		open: false,
+		type: "create",
+	});
+	const [truckData, setTruckData] = useState<{
+		_id: string;
+		custom_id: string;
+		plate_number: string;
+		group_id: string | undefined;
+		driver_id: string;
+		last_oil_change?: string | undefined;
+		last_wash?: string | undefined;
+	}>({
+		_id: "",
+		custom_id: "",
+		plate_number: "",
+		group_id: "",
+		driver_id: "",
+		last_oil_change: "",
+		last_wash: "",
+	});
+
+	const activateCreateModal = () => {
+
+		setTruckData({
+			...truckData,
+			_id: "",
+			custom_id: "",
+			plate_number: "",
+			group_id: user?.group_id,
+			driver_id: driverList[0]._id,
+			last_oil_change: "",
+			last_wash: "",
+		});
+		setCreateModalState({
+			...createModalState,
+			open: true,
+			type: "create",
+		});
+	};
+
+	const createTruck = async () => {
+		const { _id, ...restData } = truckData;
+
+		const asArray = Object.entries(restData);
+
+		const filtered = asArray.filter(
+			([key, value]) => value !== null && value !== ""
+		);
+
+		const finalData = Object.fromEntries(filtered);		
+
+		try {
+			const response = await sendRequest({
+				method: "POST",
+				route: `trucks`,
+				body: finalData,
+				token,
+			});
+
+			if (response.status === 200) {
+				setCreateModalState({ ...createModalState, open: false });
+				getTrucks();
+				toast.success("Truck created successfully", { duration: 2500 });
+			} else {
+				setCreateModalState({ ...createModalState, open: false });
+				toast.error("Couldn't Create, Try Again", { duration: 4000 });
+			}
+		} catch (err: any) {
+			console.error(err);
+			setCreateModalState({ ...createModalState, open: false });
+			toast.error("Couldn't Create, Try Again", { duration: 2500 });
+		}
+	};
+
+	const transformedDriversList = driverList.reduce(
+		(acc: Record<string, string>, currentItem) => {
+			acc[currentItem.username] = currentItem._id;
+			return acc;
+		},
+		{}
+	);
+
 	return (
 		<div className="flex">
 			<Sidebar
@@ -84,6 +191,114 @@ export default function AdminTrucks() {
 				]}
 				selected="Trucks"
 			/>
+			{/* Create - Edit Modal */}
+			<ModalComponent
+				showModal={createModalState.open}
+				onRequestClose={() =>
+					setCreateModalState({
+						...createModalState,
+						open: !createModalState.open,
+					})
+				}
+			>
+				<div className="text-xl">Create/Edit User</div>
+				<div className="flex flex-col flex-wrap justify-center content-center w-96">
+					<div className="flex gap-5">
+						<Input
+							label="Custom ID"
+							placeholder="id"
+							value={truckData.custom_id}
+							onChange={(e) => {
+								setTruckData({
+									...truckData,
+									custom_id: e.target.value,
+								}); console.log(truckData);
+								
+							}}
+							required
+						/>
+						<Input
+							label="Plate Number"
+							placeholder="plate"
+							value={truckData.plate_number}
+							onChange={(e) => {
+								setTruckData({
+									...truckData,
+									plate_number: e.target.value,
+								}); console.log(truckData);
+							}}
+							required
+						/>
+					</div>
+					<div className="flex gap-5">
+						<Input
+							label="Last Oil Change"
+							type="date"
+							value={truckData.last_oil_change}
+							onChange={(e) => {
+								setTruckData({
+									...truckData,
+									last_oil_change: e.target.value,
+								}); console.log(truckData);
+							}}
+							required
+						/>
+						<Input
+							label="Last Wash"
+							type="date"
+							value={truckData.last_wash}
+							onChange={(e) => {
+								setTruckData({
+									...truckData,
+									last_wash: e.target.value,
+								}); console.log(truckData);
+							}}
+							required
+						/>
+					</div>
+					<Select
+						label="Driver"
+						required
+						value={truckData.driver_id}
+						options={transformedDriversList}
+						onChange={(e) =>
+							setTruckData({
+								...truckData,
+								driver_id: e.target.value,
+							})
+						}
+					/>
+				</div>
+				<div className="flex w-full justify-center gap-10 mt-5">
+					<Button
+						label="Cancel"
+						color="text-gunmetal"
+						bgColor="bg-neutral-100"
+						hoverColor="hover:bg-neutral-600"
+						onClick={() =>
+							setCreateModalState({
+								...createModalState,
+								open: false,
+							})
+						}
+					/>
+					{createModalState.type === "edit" ? (
+						<Button
+							label="Update"
+							bgColor="bg-primary-500"
+							hoverColor="hover:bg-primary-700"
+							// onClick={() => updateUser()}
+						/>
+					) : (
+						<Button
+							label="Create"
+							bgColor="bg-primary-500"
+							hoverColor="hover:bg-primary-700"
+							onClick={() => createTruck()}
+						/>
+					)}
+				</div>
+			</ModalComponent>
 			{/* Delete Modal */}
 			<ModalComponent
 				showModal={deleteModalState.open}
@@ -117,7 +332,11 @@ export default function AdminTrucks() {
 				</div>
 			</ModalComponent>
 			<div className="flex flex-col w-full">
-				<Navbar label="Trucks" />
+				<Navbar
+					label="Trucks"
+					buttonLabel="+ Create Truck"
+					buttonAction={() => activateCreateModal()}
+				/>
 				<div>
 					<Toaster />
 				</div>
