77f510738d08c5febac22425144001a7b4ef0f33
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-09-26 19:11:49 +0300

    <Web> updated dashboard to fetch data

--- Web/src/dist/pages/admin/dashboard/index.js
+++ Web/src/dist/pages/admin/dashboard/index.js
@@ -1,15 +1,68 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const react_1 = __importDefault(require("react"));
+const react_1 = __importStar(require("react"));
 const react_redux_1 = require("react-redux");
 const sidebar_1 = __importDefault(require("../../../components/common/sidebar"));
 const navbar_1 = __importDefault(require("../../../components/common/navbar"));
+const request_1 = require("../../../configs/request");
 function AdminDashboard() {
     const user = (0, react_redux_1.useSelector)((state) => state.auth.user);
+    const token = (0, react_redux_1.useSelector)((state) => state.auth.token);
     const collapse = (0, react_redux_1.useSelector)((state) => state.sidebar.collapse);
+    const [stats, setStats] = (0, react_1.useState)({});
+    const getAnalytics = () => __awaiter(this, void 0, void 0, function* () {
+        try {
+            const response = yield (0, request_1.sendRequest)({
+                route: `analytics`,
+                token,
+            });
+            if (response.status === 200) {
+                setStats(response.data);
+                console.log(stats);
+            }
+        }
+        catch (err) {
+            console.log(err);
+        }
+    });
+    (0, react_1.useEffect)(() => {
+        getAnalytics();
+    }, []);
     return (react_1.default.createElement("div", { className: "flex" },
         react_1.default.createElement(sidebar_1.default, { items: [
                 "Dashboard",
@@ -23,11 +76,6 @@ function AdminDashboard() {
             ], selected: "Dashboard" }),
         react_1.default.createElement("div", { className: `flex flex-col w-full ${collapse ? "ml-20" : "ml-52"}` },
             react_1.default.createElement(navbar_1.default, { label: "Dashboard" }),
-            react_1.default.createElement("div", { className: "p-5" },
-                react_1.default.createElement("h2", null,
-                    "Hello ", user === null || user === void 0 ? void 0 :
-                    user.first_name,
-                    " ", user === null || user === void 0 ? void 0 :
-                    user.last_name)))));
+            react_1.default.createElement("div", { className: "p-5" }))));
 }
 exports.default = AdminDashboard;
--- Web/src/pages/admin/dashboard/index.tsx
+++ Web/src/pages/admin/dashboard/index.tsx
@@ -1,18 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../store/store";
-import { User } from "../../../store/interfaces";
+import { Token, User } from "../../../store/interfaces";
 import Sidebar from "../../../components/common/sidebar";
 import Navbar from "../../../components/common/navbar";
+import { sendRequest } from "../../../configs/request";
 
 export default function AdminDashboard() {
 	const user: User | null = useSelector(
 		(state: RootState) => state.auth.user
 	);
 
+	const token: Token | null = useSelector(
+		(state: RootState) => state.auth.token
+	);
+
 	const collapse: boolean = useSelector(
 		(state: RootState) => state.sidebar.collapse
 	);
+
+	const [stats, setStats] = useState({});
+
+	const getAnalytics = async () => {
+		try {
+			const response = await sendRequest({
+				route: `analytics`,
+				token,
+			});
+			if (response.status === 200) {
+				setStats(response.data);
+				console.log(stats);
+			}
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
+	useEffect(() => {
+		getAnalytics()
+	}, [])
 
 	return (
 		<div className="flex">
@@ -35,11 +61,7 @@ export default function AdminDashboard() {
 				}`}
 			>
 				<Navbar label="Dashboard" />
-				<div className="p-5">
-					<h2>
-						Hello {user?.first_name} {user?.last_name}
-					</h2>
-				</div>
+				<div className="p-5"></div>
 			</div>
 		</div>
 	);
