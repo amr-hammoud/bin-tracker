a49652125915d47fec2b45fb2a71abdc9fa26315
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-09-26 09:39:57 +0300

    <Server> fixed password hidden bug in login page

--- Server/controllers/auth.controllers.js
+++ Server/controllers/auth.controllers.js
@@ -9,7 +9,7 @@ const login = async (req, res) => {
 	try {
 		const user = await User.findOne({
 			$or: [{ email }, { username }],
-		});
+		}).select('+password');
 		if (!user)
 			return res
 				.status(404)
