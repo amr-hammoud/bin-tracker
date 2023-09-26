fc8507c2f3daf77333dff467246be3d36a3ee00d
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-09-26 09:02:22 +0300

    <Web> created a sidebar slice to handle collapse state

--- Web/src/dist/store/persistConfig.js
+++ Web/src/dist/store/persistConfig.js
@@ -3,11 +3,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// reduxPersistConfig.ts
 const storage_1 = __importDefault(require("redux-persist/lib/storage"));
 const redux_persist_1 = require("redux-persist");
 const redux_1 = require("redux");
 const authSlice_1 = __importDefault(require("./authSlice"));
+const sidebarSlice_1 = __importDefault(require("./sidebarSlice"));
 const persistConfig = {
     key: "root",
     storage: storage_1.default,
@@ -15,6 +15,7 @@ const persistConfig = {
 };
 const rootReducer = (0, redux_1.combineReducers)({
     auth: authSlice_1.default,
+    sidebar: sidebarSlice_1.default,
 });
 const persistedReducer = (0, redux_persist_1.persistReducer)(persistConfig, rootReducer);
 exports.default = persistedReducer;
