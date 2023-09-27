b9e00c4fba65f3e75a33e682cb9688211ab5c8d9
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-09-27 16:38:44 +0300

    <Web> added dashboard counters

--- Web/src/dist/pages/admin/dashboard/index.js
+++ Web/src/dist/pages/admin/dashboard/index.js
@@ -44,7 +44,22 @@ function AdminDashboard() {
     const user = (0, react_redux_1.useSelector)((state) => state.auth.user);
     const token = (0, react_redux_1.useSelector)((state) => state.auth.token);
     const collapse = (0, react_redux_1.useSelector)((state) => state.sidebar.collapse);
-    const [stats, setStats] = (0, react_1.useState)({});
+    const [stats, setStats] = (0, react_1.useState)({
+        users_count: 0,
+        admins_count: 0,
+        drivers_count: 0,
+        bins_count: 0,
+        general_bins_count: 0,
+        recyclables_bins_count: 0,
+        hazardous_bins_count: 0,
+        trucks_count: 0,
+        collected_bins_per_day: [
+            {
+                date: new Date().toISOString(),
+                count: 0,
+            },
+        ],
+    });
     const getAnalytics = () => __awaiter(this, void 0, void 0, function* () {
         try {
             const response = yield (0, request_1.sendRequest)({
@@ -53,7 +68,6 @@ function AdminDashboard() {
             });
             if (response.status === 200) {
                 setStats(response.data);
-                console.log(stats);
             }
         }
         catch (err) {
@@ -76,6 +90,39 @@ function AdminDashboard() {
             ], selected: "Dashboard" }),
         react_1.default.createElement("div", { className: `flex flex-col w-full ${collapse ? "ml-20" : "ml-52"}` },
             react_1.default.createElement(navbar_1.default, { label: "Dashboard" }),
-            react_1.default.createElement("div", { className: "p-5" }))));
+            react_1.default.createElement("div", { className: "p-5" },
+                react_1.default.createElement("div", { className: "flex w-full flex-wrap md:flex-nowrap justify-center gap-16 h-fit text-gunmetal" },
+                    react_1.default.createElement("div", { className: "flex flex-col w-full h-full gap-2" },
+                        react_1.default.createElement("div", { className: "flex flex-col w-full h-full gap-2" },
+                            react_1.default.createElement("div", { className: "flex flex-col flex-wrap justify-center content-center h-full w-full bg-primary-100 hover:bg-primary-200 p-3 text-center rounded" },
+                                react_1.default.createElement("div", { className: " text-xl" }, "Users\u00A0Count"),
+                                react_1.default.createElement("div", { className: "mt-5 font-bold text-4xl" }, stats === null || stats === void 0 ? void 0 : stats.users_count)),
+                            react_1.default.createElement("div", { className: "flex flex-wrap sm:flex-nowrap h-full gap-2" },
+                                react_1.default.createElement("div", { className: "flex flex-col flex-wrap justify-center content-center w-full h-full bg-neutral-50 hover:bg-neutral-100 p-3 text-center rounded" },
+                                    react_1.default.createElement("div", { className: " text-xl" }, "Admins"),
+                                    react_1.default.createElement("div", { className: "mt-5 font-bold text-4xl" }, stats === null || stats === void 0 ? void 0 : stats.admins_count)),
+                                react_1.default.createElement("div", { className: "flex flex-col flex-wrap justify-center content-center w-full h-full bg-neutral-50 hover:bg-neutral-100 p-3 text-center rounded" },
+                                    react_1.default.createElement("div", { className: " text-xl" }, "Drivers"),
+                                    react_1.default.createElement("div", { className: "mt-5 font-bold text-4xl" }, stats === null || stats === void 0 ? void 0 : stats.drivers_count))))),
+                    react_1.default.createElement("div", { className: "flex flex-col w-full h-full gap-2" },
+                        react_1.default.createElement("div", { className: "flex flex-col w-full h-full gap-2" },
+                            react_1.default.createElement("div", { className: "flex flex-col flex-wrap justify-center content-center h-full w-full bg-primary-100 hover:bg-primary-200 p-3 text-center rounded" },
+                                react_1.default.createElement("div", { className: " text-xl" }, "Bins\u00A0Count"),
+                                react_1.default.createElement("div", { className: "mt-5 font-bold text-4xl" }, stats === null || stats === void 0 ? void 0 : stats.bins_count)),
+                            react_1.default.createElement("div", { className: "flex flex-wrap lg:flex-nowrap h-full gap-2" },
+                                react_1.default.createElement("div", { className: "flex flex-col flex-wrap justify-center content-center w-full h-full bg-neutral-50 hover:bg-neutral-100 p-3 text-center rounded" },
+                                    react_1.default.createElement("div", { className: " text-xl" }, "General"),
+                                    react_1.default.createElement("div", { className: "mt-5 font-bold text-4xl" }, stats === null || stats === void 0 ? void 0 : stats.general_bins_count)),
+                                react_1.default.createElement("div", { className: "flex flex-col flex-wrap justify-center content-center w-full h-full bg-neutral-50 hover:bg-neutral-100 p-3 text-center rounded" },
+                                    react_1.default.createElement("div", { className: " text-xl" }, "Recyclables"),
+                                    react_1.default.createElement("div", { className: "mt-5 font-bold text-4xl" }, stats === null || stats === void 0 ? void 0 : stats.recyclables_bins_count)),
+                                react_1.default.createElement("div", { className: "flex flex-col flex-wrap justify-center content-center w-full h-full bg-neutral-50 hover:bg-neutral-100 p-3 text-center rounded" },
+                                    react_1.default.createElement("div", { className: " text-xl" }, "Hazardous"),
+                                    react_1.default.createElement("div", { className: "mt-5 font-bold text-4xl" }, stats === null || stats === void 0 ? void 0 : stats.hazardous_bins_count))))),
+                    react_1.default.createElement("div", { className: "flex flex-col w-full h-full gap-2" },
+                        react_1.default.createElement("div", { className: "flex flex-col w-full h-full gap-2" },
+                            react_1.default.createElement("div", { className: "flex flex-col flex-wrap justify-center content-center h-full w-full bg-primary-100 hover:bg-primary-200 p-3 text-center rounded" },
+                                react_1.default.createElement("div", { className: " text-xl" }, "Trucks\u00A0Count"),
+                                react_1.default.createElement("div", { className: "mt-5 font-bold text-4xl" }, stats === null || stats === void 0 ? void 0 : stats.trucks_count)))))))));
 }
 exports.default = AdminDashboard;
--- Web/src/pages/admin/dashboard/index.tsx
+++ Web/src/pages/admin/dashboard/index.tsx
@@ -19,7 +19,35 @@ export default function AdminDashboard() {
 		(state: RootState) => state.sidebar.collapse
 	);
 
-	const [stats, setStats] = useState({});
+	const [stats, setStats] = useState<{
+		users_count: number;
+		admins_count: number;
+		drivers_count: number;
+		bins_count: number;
+		general_bins_count: number;
+		recyclables_bins_count: number;
+		hazardous_bins_count: number;
+		trucks_count: number;
+		collected_bins_per_day: Array<{
+			date: string;
+			count: number;
+		}>;
+	}>({
+		users_count: 0,
+		admins_count: 0,
+		drivers_count: 0,
+		bins_count: 0,
+		general_bins_count: 0,
+		recyclables_bins_count: 0,
+		hazardous_bins_count: 0,
+		trucks_count: 0,
+		collected_bins_per_day: [
+			{
+				date: new Date().toISOString(),
+				count: 0,
+			},
+		],
+	});
 
 	const getAnalytics = async () => {
 		try {
@@ -29,7 +57,6 @@ export default function AdminDashboard() {
 			});
 			if (response.status === 200) {
 				setStats(response.data);
-				console.log(stats);
 			}
 		} catch (err) {
 			console.log(err);
@@ -37,8 +64,8 @@ export default function AdminDashboard() {
 	};
 
 	useEffect(() => {
-		getAnalytics()
-	}, [])
+		getAnalytics();
+	}, []);
 
 	return (
 		<div className="flex">
@@ -61,7 +88,84 @@ export default function AdminDashboard() {
 				}`}
 			>
 				<Navbar label="Dashboard" />
-				<div className="p-5"></div>
+				<div className="p-5">
+					<div className="flex w-full flex-wrap md:flex-nowrap justify-center gap-16 h-fit text-gunmetal">
+						<div className="flex flex-col w-full h-full gap-2">
+							<div className="flex flex-col w-full h-full gap-2">
+								<div className="flex flex-col flex-wrap justify-center content-center h-full w-full bg-primary-100 hover:bg-primary-200 p-3 text-center rounded">
+									<div className=" text-xl">
+										Users&nbsp;Count
+									</div>
+									<div className="mt-5 font-bold text-4xl">
+										{stats?.users_count}
+									</div>
+								</div>
+								<div className="flex flex-wrap sm:flex-nowrap h-full gap-2">
+									<div className="flex flex-col flex-wrap justify-center content-center w-full h-full bg-neutral-50 hover:bg-neutral-100 p-3 text-center rounded">
+										<div className=" text-xl">Admins</div>
+										<div className="mt-5 font-bold text-4xl">
+											{stats?.admins_count}
+										</div>
+									</div>
+									<div className="flex flex-col flex-wrap justify-center content-center w-full h-full bg-neutral-50 hover:bg-neutral-100 p-3 text-center rounded">
+										<div className=" text-xl">Drivers</div>
+										<div className="mt-5 font-bold text-4xl">
+											{stats?.drivers_count}
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div className="flex flex-col w-full h-full gap-2">
+							<div className="flex flex-col w-full h-full gap-2">
+								<div className="flex flex-col flex-wrap justify-center content-center h-full w-full bg-primary-100 hover:bg-primary-200 p-3 text-center rounded">
+									<div className=" text-xl">
+										Bins&nbsp;Count
+									</div>
+									<div className="mt-5 font-bold text-4xl">
+										{stats?.bins_count}
+									</div>
+								</div>
+								<div className="flex flex-wrap lg:flex-nowrap h-full gap-2">
+									<div className="flex flex-col flex-wrap justify-center content-center w-full h-full bg-neutral-50 hover:bg-neutral-100 p-3 text-center rounded">
+										<div className=" text-xl">General</div>
+										<div className="mt-5 font-bold text-4xl">
+											{stats?.general_bins_count}
+										</div>
+									</div>
+									<div className="flex flex-col flex-wrap justify-center content-center w-full h-full bg-neutral-50 hover:bg-neutral-100 p-3 text-center rounded">
+										<div className=" text-xl">
+											Recyclables
+										</div>
+										<div className="mt-5 font-bold text-4xl">
+											{stats?.recyclables_bins_count}
+										</div>
+									</div>
+									<div className="flex flex-col flex-wrap justify-center content-center w-full h-full bg-neutral-50 hover:bg-neutral-100 p-3 text-center rounded">
+										<div className=" text-xl">
+											Hazardous
+										</div>
+										<div className="mt-5 font-bold text-4xl">
+											{stats?.hazardous_bins_count}
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div className="flex flex-col w-full h-full gap-2">
+							<div className="flex flex-col w-full h-full gap-2">
+								<div className="flex flex-col flex-wrap justify-center content-center h-full w-full bg-primary-100 hover:bg-primary-200 p-3 text-center rounded">
+									<div className=" text-xl">
+										Trucks&nbsp;Count
+									</div>
+									<div className="mt-5 font-bold text-4xl">
+										{stats?.trucks_count}
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
