e942f3a1b8f826cd9c8c2fd3572bae287fed183c
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-09-28 17:33:20 +0300

    <Web> created e401 page

---
+++ Web/src/pages/other/e401.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import logo from "../../assets/logo/Logo-full.svg";
+import Button from "../../components/base/button";
+import { useNavigate } from "react-router-dom";
+
+export default function E401Page() {
+	const navigate = useNavigate();
+
+	return (
+		<div className="flex flex-wrap flex-col justify-center content-center h-screen bg-neutral-50 font-poppins text-gunmetal">
+			<div className="bg-neutral-0 p-10 rounded-md flex flex-wrap lg:flex-nowrap mx-10 max-w-md lg:max-w-none gap-5 drop-shadow-2xl">
+				<div className="w-full max-w-md flex flex-col flex-wrap gap-10">
+					<img src={logo} alt="" />
+					<h1 className=" text-center text-2xl font-semibold">Sorry, you don't have permission<br/>to access this page!</h1>
+					<Button label="Go Back" onClick={() => navigate(-1)} />
+				</div>
+			</div>
+		</div>
+	);
+}
