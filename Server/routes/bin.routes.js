60af170771f802fdeae88bfee66952c18e752ffe
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-09-29 14:50:08 +0300

    <Server> added route for pickup stamp controller

--- Server/routes/bin.routes.js
+++ Server/routes/bin.routes.js
@@ -7,6 +7,7 @@ router.post("/best-route", authMiddleware, binController.calculateOptimalRoute)
 router.get("/:id?", authMiddleware, binController.getBin)
 router.post("/:id?", authMiddleware, binController.createOrUpdateBin)
 router.delete("/:id", authMiddleware, binController.deleteBin)
+router.get("/:id/stamp/", binController.addPickupStamp)
 router.post("/:id/records/", binController.addBinRecord)
 router.delete("/:bin_id/records/:record_id", binController.deleteBinRecord)
 
