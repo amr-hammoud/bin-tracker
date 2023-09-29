e416517bd35a7f6cae792a647d2a531b51b757d7
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-09-29 07:43:46 +0300

    <Server> changed middleware on get user route

--- Server/routes/user.routes.js
+++ Server/routes/user.routes.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/user.controllers");
-const superAdminMiddleware = require("../middlewares/superadmin.middleware");
 const authMiddleware = require("../middlewares/auth.middleware");
 
 router.get("/drivers", authMiddleware, userController.getGroupDrivers)
 router.get("/group/:id?", authMiddleware, userController.getGroupUser)
-router.get("/:id?", superAdminMiddleware, userController.getUser)
+router.get("/:id?", authMiddleware, userController.getUser)
 router.put("/:id", authMiddleware, userController.updateUser)
 router.delete("/:id", authMiddleware, userController.deleteUser)
 
